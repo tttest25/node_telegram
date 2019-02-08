0d3b5494c6560a86b26ade79695a2834b3287807
Eugen MElnikoff <emelnikoff@gmail.com>
2019-02-08 15:36:46 +0000

    # First fucntional mockup

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const session = require('telegraf/session')
 const Markup = require('telegraf/markup')
 const { reply } = Telegraf
 
-
 const configObj = require('../config.json');
 
 const environment = process.env.NODE_ENV || 'development';
@@ -26,7 +25,13 @@ global.gConfig = finalConfig;
 console.log(`global.gConfig: ${JSON.stringify(global.gConfig, undefined, global.gConfig.json_indentation)}`);
 
 
+const util = require('./helpers/util');
+const dataService = require('./helpers/dataService');
+
+dataService.loadUsers();
+
 const bot = new Telegraf(process.env.BOT_TOKEN || global.gConfig.bot_token )
+util.setBot(bot);
 
 // // Register session middleware
 bot.use(session())
@@ -44,11 +49,26 @@ bot.catch((err) => {
   console.error('Error', err)
 })
 
-
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.command('start', ctx => {
+    util.logMsg(ctx);
+    dataService.registerUser(ctx);
+    //dataService.setCounter(ctx.chat.id, '0', 0);
+    var m = "Hello, I'm your AGP bot, please ask admin to add you to group";
+    ctx.reply(m);
+    util.logOutMsg(ctx, m);
+    setTimeout(() => {
+        ctx.reply(0);
+        util.logOutMsg(ctx, 0)
+    }, 50); //workaround to send this message definitely as second message
+});
+//bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+bot.hears('log', (ctx) => {
+  ctx.reply('See log!');
+  util.logMsg(ctx);
+})
 bot.launch()
 
 
@@ -60,8 +80,19 @@ const HOST = global.gConfig.host;
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello 4 world\n');
+  util.sendToAdmin('test send to admin');
 });
 
 
+process.on('unhandledRejection', (e) => {
+  console.log(e);
+//   sendToAdmin(`Unhandled Rejection! ${e.message}`);
+});
+
+process.on('uncaughtException', (e) => {
+  console.log(e);
+//   sendToAdmin(`Uncaught Exception! ${e.message}`);
+});
+
 app.listen(PORT, HOST);
-console.log(`Running 3 on http://${HOST}:${PORT}`);
+console.log(`-> Running 3 on http://${HOST}:${PORT}`);
